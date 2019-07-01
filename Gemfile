2b950f476aa1de03f5e97ace757dcb47cb89df6e
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-07-01 13:57:39 +1000

    authentication feature added

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # gems added for my project
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,86 @@
+class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
+
+  # GET /cars
+  # GET /cars.json
+  def index
+    @cars = Car.all
+  end
+
+  # GET /cars/1
+  # GET /cars/1.json
+  def show
+  end
+
+  # GET /cars/new
+  def new
+    @car = Car.new
+    # if the user is not logged in redirect to the login page
+    # else redirect to the cars form page
+    if user_signed_in?
+      # if the user has create a profile show the cars form else redirect to creating a profile page
+       if current_user.profile
+        # render cars form page
+       else
+         redirect_to new_profile_path
+       end
+    else
+        redirect_to new_user_session_path
+    end
+  end
+
+  # GET /cars/1/edit
+  def edit
+  end
+
+  # POST /cars
+  # POST /cars.json
+  def create
+    @car = Car.new(car_params)
+
+    respond_to do |format|
+      if @car.save
+        format.html { redirect_to @car, notice: 'Car was successfully created.' }
+        format.json { render :show, status: :created, location: @car }
+      else
+        format.html { render :new }
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /cars/1
+  # PATCH/PUT /cars/1.json
+  def update
+    respond_to do |format|
+      if @car.update(car_params)
+        format.html { redirect_to @car, notice: 'Car was successfully updated.' }
+        format.json { render :show, status: :ok, location: @car }
+      else
+        format.html { render :edit }
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /cars/1
+  # DELETE /cars/1.json
+  def destroy
+    @car.destroy
+    respond_to do |format|
+      format.html { redirect_to cars_url, notice: 'Car was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_car
+      @car = Car.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def car_params
+      params.require(:car).permit(:picture, :make, :model, :year, :km, :price, :color, :registration, :description, :fuel_type, :transmission_type, :location)
+    end
+end
